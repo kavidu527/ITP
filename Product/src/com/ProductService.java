3602a4dd914742875de84275152ca446c930e030
dell-pc <dell-pc@DESKTOP-4DAGNUC>
2021-04-23 17:50:26 +0530

    THe first insert

---
+++ Product/src/com/ProductService.java
@@ -0,0 +1,43 @@
+package com;
+
+
+import model.Product;
+//For REST Service
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+//For JSON
+import com.google.gson.*;
+//For XML
+import org.jsoup.*;
+import org.jsoup.parser.*;
+import org.jsoup.nodes.Document;
+@Path("/Products")
+
+public class ProductService {
+
+	Product productObj = new Product();
+	@GET
+	@Path("/")
+	@Produces(MediaType.TEXT_HTML)
+	
+	public String readProducts()
+	 {
+		return productObj.readProducts(); 
+	 }
+	
+	@POST
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String insertProduct
+	(@FormParam("productCode") String productCode,
+	 @FormParam("productName") String productName,
+	 @FormParam("productPrice") String productPrice,
+	 @FormParam("productDesc") String productDesc)
+	{
+	 String output = productObj.insertProduct(productCode, productName, productPrice, productDesc);
+	return output;
+	}
+	
+	
+}
